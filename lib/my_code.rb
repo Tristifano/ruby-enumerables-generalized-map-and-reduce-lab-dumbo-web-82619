e4a8952303fc0274ee18bf4815a8ddeb0a67366b
Tristan Myers <tristifano@gmail.com>
2019-08-06 17:43:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+def map(source_array) 
+  index = 0
+  source_array.each do |number|
+    source_array[index] = yield(number)
+    index += 1
+  end
+  source_array
+end
+
+def reduce(source_array, starting_point = 0)
+  value = starting_point
+  source_array.each do |element|
+    value = yield(value, element)
+  end
+  value
+end
